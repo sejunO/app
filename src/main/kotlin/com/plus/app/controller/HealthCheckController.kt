8b6ee3e287c28e83369dca62627db1086f829d25
osejun <frame8916@gmail.com>
2023-12-04 21:22:31 +0900

    init

---
+++ src/main/kotlin/com/plus/app/controller/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package com.plus.app.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+
+    @GetMapping("/health-check")
+    fun healthCheck() {}
+
+}
